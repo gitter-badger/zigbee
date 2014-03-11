e328c9ce16f5665e0cbfb660deb71f7e1e0abfcd
rlqmal <rlqmal@gmail.com>
2014-03-11 20:19:47 +0900

    change build folder

--- build2/swig/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kibum/gr-ieee802-15-4/swig
-# Build directory: /home/kibum/gr-ieee802-15-4/build/swig
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
